463e11cbd431c2329f91eda65bd9ea4cdc1d1b05
Vahid Ghanbarpour <vahidgr.vahid@gmail.com>
2019-10-24 13:33:07 +0330

    Initial Commit

---
+++ Profilio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Profilio
+//
+//  Created by Vahid Ghanbarpour on 10/24/19.
+//  Copyright © 2019 Vahid Ghanbarpour. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
